548f350f8db8ce1886aab2dbec796837ba744b40
Eng Kamal Abu Zarifa <144855748+kamalabuzarifa@users.noreply.github.com>
2024-12-26 09:43:05 +0200

    oncopy

--- js/master.js
+++ js/master.js
@@ -285,3 +285,10 @@ document.querySelector(".reset-options").onclick = function () {
     // إعادة تحميل الصفحة لتطبيق التغييرات
     window.location.reload();
 }
+
+document.oncopy = () => {
+    let text = window.getSelection().toString();
+    let url = window.location.href;
+    let final_text = text + "\n\nFor More Information" + url;
+    navigator.clipboard.writeText(final_text);
+};
